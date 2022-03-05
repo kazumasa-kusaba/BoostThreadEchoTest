fc3b066fe1ea7e56d3756610d1146418d9c49419
Kazumasa Kusaba <kazumasa@mail.kusaba.xyz>
2022-03-05 23:59:06 +0900

    tempolary commit

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <boost/thread/thread.hpp>
 #include <boost/interprocess/ipc/message_queue.hpp>
 
+const std::string MAIN_UNIQUE_KEY              = "MAIN_UNIQUE_KEY";
+const int         MAIN_MAX_MESSAGE_SIZE        = 500;
 const std::string ECHO_CLIENT_UNIQUE_KEY       = "ECHO_CLIENT_UNIQUE_KEY";
 const int         ECHO_CLIENT_MAX_MESSAGE_SIZE = 500;
-const std::string MAIN_UNIQUE_KEY              = "MAIN_UNIQUE_KEY";
-const int         MAIN_MAX_MESSAGE_SIZE        = 4;
 
 enum COMMAND {
   SEND_MESSAGE = 0,
@@ -21,17 +21,33 @@ struct MESSAGE {
 void echo_client_function(void)
 {
   try {
-    boost::interprocess::message_queue msgq(boost::interprocess::open_only, 
-                                            ECHO_CLIENT_UNIQUE_KEY.c_str()); 
-    MESSAGE      message;
+    boost::interprocess::message_queue echo_client_msgq(boost::interprocess::open_only, 
+                                                        ECHO_CLIENT_UNIQUE_KEY.c_str()); 
+    boost::interprocess::message_queue main_msgq(boost::interprocess::open_only, 
+                                                 MAIN_UNIQUE_KEY.c_str()); 
+    MESSAGE      message_to_echo_client, message_to_main;
     size_t       recv_size = 0;
     unsigned int priority  = 0;
 
     while (1) {
       // receive message
-      msgq.receive(&message, sizeof(MESSAGE), recv_size, priority);
-      std::cout << "command: " << message.command << std::endl;
-      std::cout << "message: " << message.message << std::endl;
+      echo_client_msgq.receive(&message_to_echo_client, sizeof(MESSAGE), recv_size, priority);
+
+      std::cout << "test" << std::endl;
+      
+      // handle message
+      switch (message_to_echo_client.command) {
+        case SEND_MESSAGE:
+          message_to_main.command = EXIT_THREAD;
+          message_to_main.message = message_to_echo_client.message;
+          std::reverse(message_to_main.message.begin(), message_to_main.message.end());
+          main_msgq.send(&message_to_main, sizeof(message_to_main), 0);
+          break;
+        case EXIT_THREAD:
+          return;
+        default:
+          break;
+      }
     }
   } catch (boost::interprocess::interprocess_exception& e) {
       std::cerr << e.what() << std::endl;
@@ -44,30 +60,43 @@ void echo_client_function(void)
 
 int main(void)
 {
-  // create message queue
   try {
     // if the message queue left, it needs to be removed.
     boost::interprocess::message_queue::remove(ECHO_CLIENT_UNIQUE_KEY.c_str());
-    boost::interprocess::message_queue msgq(
-      boost::interprocess::create_only, 
-      ECHO_CLIENT_UNIQUE_KEY.c_str(),
-      ECHO_CLIENT_MAX_MESSAGE_SIZE,
-      sizeof(MESSAGE));
+    // create message queue
+    boost::interprocess::message_queue echo_client_msgq(boost::interprocess::create_only, 
+                                                        ECHO_CLIENT_UNIQUE_KEY.c_str(),
+                                                        ECHO_CLIENT_MAX_MESSAGE_SIZE,
+                                                        sizeof(MESSAGE));
+
+    // if the message queue left, it needs to be removed.
+    boost::interprocess::message_queue::remove(MAIN_UNIQUE_KEY.c_str());
+    // create message queue
+    boost::interprocess::message_queue main_msgq(boost::interprocess::create_only, 
+                                                 MAIN_UNIQUE_KEY.c_str(),
+                                                 MAIN_MAX_MESSAGE_SIZE,
+                                                 sizeof(MESSAGE));
 
     // start thread
     boost::thread thread_echo_client(&echo_client_function);
 
-    while (1) {
-      std::string buf;
+    std::string  buf;
+    MESSAGE      message_to_echo_client, message_to_main;
+    size_t       recv_size = 0;
+    unsigned int priority  = 0;
 
+    while (1) {
       std::cout << "Input: ";
       std::cin  >> buf;
 
-      MESSAGE message;
-      message.command = SEND_MESSAGE;
-      message.message = buf;
+      message_to_echo_client.command = SEND_MESSAGE;
+      message_to_echo_client.message = buf;
 
-      msgq.send(&message, sizeof(message), 0);
+      echo_client_msgq.send(&message_to_echo_client, sizeof(message_to_echo_client), 0);
+
+      main_msgq.receive(&message_to_main, sizeof(MESSAGE), recv_size, priority);
+      std::cout << "command: " << message_to_main.command << std::endl;
+      std::cout << "message: " << message_to_main.message << std::endl;
     }
 
   } catch (boost::interprocess::interprocess_exception& e) {
